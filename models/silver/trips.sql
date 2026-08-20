cb7c9f28cbf7af253148bbaefa8879e871741d18
Manikandan Dhandayuthabani <manikandand4466@gmail.com>
2026-08-20 13:08:07 +0200

    Add files via upload

---
+++ models/silver/trips.sql
@@ -0,0 +1,19 @@
+{{
+    config(
+        materialized = 'incremental',
+        unique_key = 'trip_id'
+        )
+}}
+{% set cols = ['trip_id', 'driver_id', 'customer_id', 'vehicle_id', 'trip_start_time', 'trip_end_time', 'distance_km', 'fare_amount', 'last_updated_timestamp'] %}
+
+SELECT
+    {% for i in cols%}
+        {{ i }}
+        {% if not loop.last %}, {% endif %}
+    {% endfor %}
+FROM
+    {{ source('source_bronze', 'trips') }}
+
+{% if is_incremental() %}
+    where 'last_updated_timestamp' > (select coalesce(max('last_updated_timestamp'), '1900-01-01') from {{ this }})
+{% endif %}
